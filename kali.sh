37b2f3386ca3e7e7f51d81efeb039f00af659c79
Tondo <aliss.kuper2@gmail.com>
2026-04-19 09:07:28 +0300

    kali.sh

--- kali.sh
+++ kali.sh
@@ -2,7 +2,7 @@ sudo apt update -y
 sudo apt install python3 python3-pip snapd -y
 sudo apt install nmap -y 
 sudo apt install dirb -y
-sudo apt install nikto -y
+sudo apt -y install nikto
 sudo snap install spiderfoot
 sudo ln -s /snap/bin/spiderfoot /usr/local/bin/spiderfoot
 sudo apt install wireshark -y 
