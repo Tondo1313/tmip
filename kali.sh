77dee6aad47072feb8b58029994fada5d5631298
Tondo <aliss.kuper2@gmail.com>
2026-04-19 08:52:47 +0300

    Add files via upload

---
+++ kali.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt install python3 python3-pip snapd -y
+sudo apt install nmap -y 
+sudo apt install dirb -y
+sudo apt install nikto -y
+sudo snap install spiderfoot
+sudo ln -s /snap/bin/spiderfoot /usr/local/bin/spiderfoot
+sudo apt install burpsuit -y
+sudo apt install wireshark -y 
+sudo apt install john hashcat aircrack-ng sqlmap hydra theharvester maltego metasploit-framework-y
