c5de61096a4871191482c7d1282e05c4f1c338cb
Tondo <aliss.kuper2@gmail.com>
2026-04-19 09:08:43 +0300

    kali.sh

--- kali.sh
+++ kali.sh
@@ -2,8 +2,9 @@ sudo apt update -y
 sudo apt install python3 python3-pip snapd -y
 sudo apt install nmap -y 
 sudo apt install dirb -y
-sudo apt -y install nikto
+sudo snap install nikto
 sudo snap install spiderfoot
 sudo ln -s /snap/bin/spiderfoot /usr/local/bin/spiderfoot
 sudo apt install wireshark -y 
-sudo apt install john hashcat aircrack-ng sqlmap hydra maltego metasploit-framework -y
+sudo apt install john hashcat aircrack-ng sqlmap hydra maltego -y
+sudo snap install metasploit-framework -y
