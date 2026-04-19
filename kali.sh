bb218c435b38eb8d283c54d967ee7d5e4d43b959
Tondo <aliss.kuper2@gmail.com>
2026-04-19 09:03:52 +0300

    kali.sh

--- kali.sh
+++ kali.sh
@@ -5,6 +5,5 @@ sudo apt install dirb -y
 sudo apt install nikto -y
 sudo snap install spiderfoot
 sudo ln -s /snap/bin/spiderfoot /usr/local/bin/spiderfoot
-sudo apt install burpsuit -y
 sudo apt install wireshark -y 
-sudo apt install john hashcat aircrack-ng sqlmap hydra theharvester maltego metasploit-framework-y
+sudo apt install john hashcat aircrack-ng sqlmap hydra theharvester maltego metasploit-framework -y
